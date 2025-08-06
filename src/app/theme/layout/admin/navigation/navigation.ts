89d5e799cd85b12f77edc51a63e6857921017b8f
vinay <vinay.singh@richminds.co>
2025-08-06 17:44:09 +0530

    added

--- src/app/theme/layout/admin/navigation/navigation.ts
+++ src/app/theme/layout/admin/navigation/navigation.ts
@@ -42,118 +42,118 @@ const NavigationItems = [
       }
     ]
   },
-  {
-    id: 'ui-element',
-    title: 'UI ELEMENT',
-    type: 'group',
-    icon: 'feather icon-layers',
-    children: [
-      {
-        id: 'basic',
-        title: 'Basic',
-        type: 'collapse',
-        icon: 'feather icon-box',
-        children: [
-          {
-            id: 'alert',
-            title: 'Alert',
-            type: 'item',
-            url: '/basic/alert'
-          },
-          {
-            id: 'button',
-            title: 'Button',
-            type: 'item',
-            url: '/basic/button'
-          },
-          {
-            id: 'badges',
-            title: 'Badges',
-            type: 'item',
-            url: '/basic/badges'
-          },
-          {
-            id: 'breadcrumb-pagination',
-            title: 'Breadcrumbs & Pagination',
-            type: 'item',
-            url: '/basic/breadcrumb-paging'
-          },
-          {
-            id: 'cards',
-            title: 'Cards',
-            type: 'item',
-            url: '/basic/cards'
-          },
-          {
-            id: 'collapse',
-            title: 'Collapse',
-            type: 'item',
-            url: '/basic/collapse'
-          },
-          {
-            id: 'carousel',
-            title: 'Carousel',
-            type: 'item',
-            url: '/basic/carousel'
-          },
-          {
-            id: 'grid-system',
-            title: 'Grid System',
-            type: 'item',
-            url: '/basic/grid-system'
-          },
-          {
-            id: 'progress',
-            title: 'Progress',
-            type: 'item',
-            url: '/basic/progress'
-          },
-          {
-            id: 'modal',
-            title: 'Modal',
-            type: 'item',
-            url: '/basic/modal'
-          },
-          {
-            id: 'spinner',
-            title: 'Spinner',
-            type: 'item',
-            url: '/basic/spinner'
-          },
-          {
-            id: 'tabs-pills',
-            title: 'Tabs & Pills',
-            type: 'item',
-            url: '/basic/tabs-pills'
-          },
-          {
-            id: 'typography',
-            title: 'Typography',
-            type: 'item',
-            url: '/basic/typography'
-          },
-          {
-            id: 'tooltip-popovers',
-            title: 'Tooltip & Popovers',
-            type: 'item',
-            url: '/basic/tooltip-popovers'
-          },
-          {
-            id: 'toasts',
-            title: 'Toasts',
-            type: 'item',
-            url: '/basic/toasts'
-          },
-          {
-            id: 'other',
-            title: 'Other',
-            type: 'item',
-            url: '/basic/other'
-          }
-        ]
-      }
-    ]
-  },
+  // {
+  //   id: 'ui-element',
+  //   title: 'UI ELEMENT',
+  //   type: 'group',
+  //   icon: 'feather icon-layers',
+  //   children: [
+  //     {
+  //       id: 'basic',
+  //       title: 'Basic',
+  //       type: 'collapse',
+  //       icon: 'feather icon-box',
+  //       children: [
+  //         {
+  //           id: 'alert',
+  //           title: 'Alert',
+  //           type: 'item',
+  //           url: '/basic/alert'
+  //         },
+  //         {
+  //           id: 'button',
+  //           title: 'Button',
+  //           type: 'item',
+  //           url: '/basic/button'
+  //         },
+  //         {
+  //           id: 'badges',
+  //           title: 'Badges',
+  //           type: 'item',
+  //           url: '/basic/badges'
+  //         },
+  //         {
+  //           id: 'breadcrumb-pagination',
+  //           title: 'Breadcrumbs & Pagination',
+  //           type: 'item',
+  //           url: '/basic/breadcrumb-paging'
+  //         },
+  //         {
+  //           id: 'cards',
+  //           title: 'Cards',
+  //           type: 'item',
+  //           url: '/basic/cards'
+  //         },
+  //         {
+  //           id: 'collapse',
+  //           title: 'Collapse',
+  //           type: 'item',
+  //           url: '/basic/collapse'
+  //         },
+  //         {
+  //           id: 'carousel',
+  //           title: 'Carousel',
+  //           type: 'item',
+  //           url: '/basic/carousel'
+  //         },
+  //         {
+  //           id: 'grid-system',
+  //           title: 'Grid System',
+  //           type: 'item',
+  //           url: '/basic/grid-system'
+  //         },
+  //         {
+  //           id: 'progress',
+  //           title: 'Progress',
+  //           type: 'item',
+  //           url: '/basic/progress'
+  //         },
+  //         {
+  //           id: 'modal',
+  //           title: 'Modal',
+  //           type: 'item',
+  //           url: '/basic/modal'
+  //         },
+  //         {
+  //           id: 'spinner',
+  //           title: 'Spinner',
+  //           type: 'item',
+  //           url: '/basic/spinner'
+  //         },
+  //         {
+  //           id: 'tabs-pills',
+  //           title: 'Tabs & Pills',
+  //           type: 'item',
+  //           url: '/basic/tabs-pills'
+  //         },
+  //         {
+  //           id: 'typography',
+  //           title: 'Typography',
+  //           type: 'item',
+  //           url: '/basic/typography'
+  //         },
+  //         {
+  //           id: 'tooltip-popovers',
+  //           title: 'Tooltip & Popovers',
+  //           type: 'item',
+  //           url: '/basic/tooltip-popovers'
+  //         },
+  //         {
+  //           id: 'toasts',
+  //           title: 'Toasts',
+  //           type: 'item',
+  //           url: '/basic/toasts'
+  //         },
+  //         {
+  //           id: 'other',
+  //           title: 'Other',
+  //           type: 'item',
+  //           url: '/basic/other'
+  //         }
+  //       ]
+  //     }
+  //   ]
+  // },
   {
     id: 'forms',
     title: 'Forms & Table',
@@ -178,70 +178,62 @@ const NavigationItems = [
       }
     ]
   },
-  {
-    id: 'chart-maps',
-    title: 'Chart & Maps',
-    type: 'group',
-    icon: 'feather icon-pie-chart',
-    children: [
-      {
-        id: 'charts',
-        title: 'Charts',
-        type: 'item',
-        url: '/charts/apex',
-        classes: 'nav-item',
-        icon: 'feather icon-pie-chart'
-      },
-      {
-        id: 'maps',
-        title: 'Maps',
-        type: 'item',
-        url: '/maps/google',
-        classes: 'nav-item',
-        icon: 'feather icon-map'
-      }
-    ]
-  },
-  {
-    id: 'pages',
-    title: 'Pages',
-    type: 'group',
-    icon: 'feather icon-file-text',
-    children: [
-      {
-        id: 'auth',
-        title: 'Authentication',
-        type: 'collapse',
-        icon: 'feather icon-lock',
-        children: [
-          {
-            id: 'signup',
-            title: 'Sign up',
-            type: 'item',
-            url: '/auth/signup',
-            target: true,
-            breadcrumbs: false
-          },
-          {
-            id: 'signin',
-            title: 'Sign in',
-            type: 'item',
-            url: '/auth/signin',
-            target: true,
-            breadcrumbs: false
-          }
-        ]
-      },
-      {
-        id: 'sample-page',
-        title: 'Sample Page',
-        type: 'item',
-        url: '/sample-page',
-        classes: 'nav-item',
-        icon: 'feather icon-sidebar'
-      }
-    ]
-  }
+  // {
+  //   id: 'chart-maps',
+  //   title: 'Chart & Maps',
+  //   type: 'group',
+  //   icon: 'feather icon-pie-chart',
+  //   children: [
+  //     {
+  //       id: 'charts',
+  //       title: 'Charts',
+  //       type: 'item',
+  //       url: '/charts/apex',
+  //       classes: 'nav-item',
+  //       icon: 'feather icon-pie-chart'
+  //     },
+  //     {
+  //       id: 'maps',
+  //       title: 'Maps',
+  //       type: 'item',
+  //       url: '/maps/google',
+  //       classes: 'nav-item',
+  //       icon: 'feather icon-map'
+  //     }
+  //   ]
+  // },
+  // {
+  //   id: 'pages',
+  //   title: 'Pages',
+  //   type: 'group',
+  //   icon: 'feather icon-file-text',
+  //   children: [
+  //     {
+  //       id: 'auth',
+  //       title: 'Authentication',
+  //       type: 'collapse',
+  //       icon: 'feather icon-lock',
+  //       children: [
+  //         {
+  //           id: 'signup',
+  //           title: 'Sign up',
+  //           type: 'item',
+  //           url: '/auth/signup',
+  //           target: true,
+  //           breadcrumbs: false
+  //         },
+  //         {
+  //           id: 'signin',
+  //           title: 'Sign in',
+  //           type: 'item',
+  //           url: '/auth/signin',
+  //           target: true,
+  //           breadcrumbs: false
+  //         }
+  //       ]
+  //     }
+  //   ]
+  // }
 ];
 
 @Injectable()
