e8360055173eabef80e060bc1efb2d24a7bc3a42
vinayRichMind <vinay.singh@richminds.co>
2025-09-30 19:49:17 +0530

    added

--- src/admin-portal/src/environments/environment.prod.ts
+++ src/admin-portal/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    rootPath : 'https://bsss-admin.onlineregistrationforms.com/api/admin'
+    rootPath : 'https://cams.onlineregistrationforms.com/api/admin'
   };
   
